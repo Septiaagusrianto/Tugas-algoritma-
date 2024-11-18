68b3163f68558ae206261e6789342516fd9eed8c
Septiaagusrianto <rianrian96603@gmail.com>
2024-11-18 19:26:24 +0800

    Delete MODUL 3 directory

--- MODUL 3/05-TPM_02/05-TPM_02/suhu.py
+++
@@ -1,8 +0,0 @@
-# input suhu dalam farenheit 
-farenheit = float(input("masukan suhu dalam farenheit"))
-
-# Mengkonfersi suhu ke celsius
-celsius = (5 /9) * (farenheit - 32)
-
-# Tampilkan hasilnya
-print(f"{farenheit} farenheit sama dengan {celsius} celsius")
