9edd844e912f17738e04e25ccb80325c8307c2b6
Septiaagusrianto <rianrian96603@gmail.com>
2024-11-18 19:27:46 +0800

    Add files via upload
    MODUL 3

---
+++ MODUL 3/01-Variable/Min.py
@@ -0,0 +1,26 @@
+#  Variabel adalah wadah yang menampung nilai / data
+
+#  cara menaruh nilai / assisment data
+   a = 10
+   x = 5
+   panjang = 1000
+
+   # memanggil variabel 
+   print("nilai a = ", a)
+   print("nilai x = ", x)
+   print("nilai panjang = ",panjang)
+
+   # penamaman variabel yang besar
+   nilai_y = 15 # menggunakan underscore 
+   juta10 =10000000 # angka tidak boleh didepan 
+   nilaiZ = 17,5 = variasi angka dengan huruf
+
+   # assimgent indirect 
+   # memberikan niali variabel dengan nilai
+   # dari variabel lain
+
+   a = 7
+   print("nilai a = ", a)
+
+   b = a
+   print("niali b = ", b)
---
+++ MODUL 3/02-TipeData/Main.py
@@ -0,0 +1,22 @@
+# integer 
+# tipe data : bilangan bualat tanpa koma
+data_integer = 11
+print("data =", data_integer)
+print("tipe data =", type data data_integer)
+
+# float
+# tipe data : bilangan desimal , dengan koma 
+data_float = 3.14
+print("data =", data_float)
+print("tipe data = ", type(data_float))
+
+# string 
+# tipe data : kumpulan karakter
+data_string = "universitas pendidikan mandalika"
+print("data =", data_string)
+print("tipe data = ", type(data_string))
+
+# boolean 
+# tipe data : logika, TRUE/FALSE
+data_bool = True
+# print 
